2493ddcf7a4973356e720b06bba930f4bb2557b5
Abel Redero <abel.red5859@gmail.com>
2026-09-16 20:07:49 +0200

    Python: ex 3 fixes

--- python/ej_3.py
+++ python/ej_3.py
@@ -1,8 +1,8 @@
 print("Give me a pair of catheti and I will give you the hypotenuse")
 
-c1 = float(input("Gimme the length of the first catheti"))
-c2 = float(input("Gimme the length of the second catheti"))
+c1 = float(input("Gimme the length of the first catheti:\n"))
+c2 = float(input("Gimme the length of the second catheti:\n"))
 
 hptn = (((c1 ** 2) + (c2 **2)) ** (1/2))
 
-print("The hypotenuse is: " + hptn)
+print("The hypotenuse is: " + str(hptn))

ec0ac9c8dc62292b74b4949ac365fd3069ddf84d
Abel Redero <abel.red5859@gmail.com>
2026-09-22 22:30:50 +0200

    Saw doors: finished and corrected

--- python/saw_doors.py
+++ python/saw_doors.py
@@ -1,16 +1,19 @@
-security_margin_x = int(input("Give me the x security margin:\n"))
-security_margin_y = int(input("Give me the y security margin:\n"))
+security_margin_x = 50
+security_margin_y = 110
 
 width = int(input("Give me the doors width:\n"))
 depth = int(input("Give me the desired depth:\n"))
 excess = int(input("Give me the desired excess:\n"))
 
-saw_radii = int(input("Give me the saw's radii:\n"))
+saw_diameter = 300
+saw_radii = saw_diameter/2
+
+saw_margin_x = ((saw_radii**2) - (saw_radii-depth)**2)**(1/2)
 
 p1 = (-security_margin_x - saw_radii, -security_margin_y - saw_radii)
-p2 = (-security_margin_x - saw_radii, depth - saw_radii)
-p3 = (width - excess - saw_radii, depth - saw_radii)
-p4 = (width - excess - saw_radii, - security_margin_y - saw_radii)
+p2 = (p1[0], depth - saw_radii)
+p3 = (width - excess - saw_margin_x, depth - saw_radii)
+p4 = (p3[0], - security_margin_y - saw_radii)
 
 print(p1)
 print(p2)
